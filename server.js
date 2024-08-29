968a6f146b2b4f90007c48c6da54b8f81e13e6c2
Atharva Tiwari <atharva.tiwari1124@gmail.com>
2024-08-29 08:24:57 -0400

    Updated Server and Website Logo

--- server.js
+++ server.js
@@ -3,12 +3,27 @@ const path = require("path");
 
 const app = express();
 
+
+/*
+This tells Express to serve static files (e.g., JavaScript, CSS) from the Client/build directory. 
+This is where the built React app files are located.
+*/
 app.use(express.static(path.join(__dirname, "Client", "build")));
 
+
+/*
+This sets up a fallback route to serve the index.html file for any requests that don't match a static file. 
+This is necessary for React Router to work correctly in single-page applications (SPA) where routing is 
+handled on the client side.
+*/
 app.use((req, res) => {
   res.sendFile(path.join(__dirname, "Client","build", "index.html"));
 });
 
+/*
+This starts the Express server, listening on a port specified by the PORT environment variable or 
+defaulting to port 3000. It logs a message when the server starts.
+*/
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server Started");
 });
